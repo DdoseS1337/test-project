c7161d34959e7fa8698a1703521025132277567e
DdoseS1337 <yaroslav.maksymchuk@gmail.com>
2022-11-17 00:03:58 +0200

    test added

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { getJWTConfig } from 'src/configs/jwt.config';
 import { Module } from '@nestjs/common';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { AuthController } from './auth.controller';
@@ -5,9 +6,7 @@ import { UserModel } from './user.model';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { getJWTConfig } from 'src/configs/jwt.config';
-import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from './strategies/jwt.strategy';
+
 @Module({
 	controllers: [AuthController],
 	imports: [
@@ -19,14 +18,12 @@ import { JwtStrategy } from './strategies/jwt.strategy';
 				},
 			},
 		]),
-		ConfigModule,
 		JwtModule.registerAsync({
 			imports: [ConfigModule],
 			inject: [ConfigService],
 			useFactory: getJWTConfig,
 		}),
-		PassportModule,
 	],
-	providers: [AuthService, JwtStrategy],
+	providers: [AuthService],
 })
 export class AuthModule {}
--- test/review.e2e-spec.ts
+++ test/review.e2e-spec.ts
@@ -5,8 +5,14 @@ import { AppModule } from './../src/app.module';
 import { CreateReviewDto } from '../src/review/dto/create-review.dto';
 import { Types, disconnect } from 'mongoose';
 import { REVIEW_NOT_FOUND } from '../src/review/review.constats';
+import { AuthDto } from 'src/auth/dto/auth.dto';
 
 const productId = new Types.ObjectId().toHexString();
+
+const loginDto: AuthDto = {
+	login: 'test@gmail.com',
+	password: 'test',
+};
 
 const testDto: CreateReviewDto = {
 	name: 'Test',
@@ -19,6 +25,7 @@ const testDto: CreateReviewDto = {
 describe('AppController (e2e)', () => {
 	let app: INestApplication;
 	let createdID: string;
+	let token: string;
 
 	beforeEach(async () => {
 		const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -27,6 +34,9 @@ describe('AppController (e2e)', () => {
 
 		app = moduleFixture.createNestApplication();
 		await app.init();
+
+		const { body } = await request(app.getHttpServer()).post('/auth/login').send(loginDto);
+		token = body.access_token;
 	});
 
 	it('/review/create (Post) - success', async () => {
@@ -71,12 +81,14 @@ describe('AppController (e2e)', () => {
 	it('/review/:id (DELETE) - success', () => {
 		request(app.getHttpServer())
 			.delete('/review/' + createdID)
-			.expect(200);
+			.expect(200)
+			.set('Authorization', 'Bearer ' + token);
 	});
 
 	it('/review/:id (DELETE) - fail', () => {
 		request(app.getHttpServer())
 			.delete('/review/' + new Types.ObjectId().toHexString())
+			.set('Authorization', 'Bearer ' + token)
 			.expect(404, {
 				statusCode: 404,
 				message: REVIEW_NOT_FOUND,
